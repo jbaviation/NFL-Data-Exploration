00258fd2f07d5642ecfd819eb563df9d48c30fc1
Jon Borman <jborman1225@gmail.com>
2022-10-01 18:32:45 -0400

    Created get_all_game_data.py and successfully scraped the data.
    This data was originally retrieved thru the use of a jupyter notebook (all_game_data.ipynb) but has since been moved into a standalone script or function that can be called.

---
+++ get_all_game_data.py
@@ -0,0 +1,124 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import re
+import time
+import datetime
+
+
+def get_all_game_score_links(soup=None):
+    """Open game scores page and retrieve appropriate links"""
+    
+    # Check if soup object is provided, if not generate one
+    if soup is None:
+        url = 'https://www.pro-football-reference.com/boxscores/game-scores.htm'
+
+        # Get page content
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+    
+    # Find table and all rows
+    table = soup.find('table', attrs={'class': 'sortable stats_table'})
+    trs = table.find_all('tr')  # each tr contains a link to each score
+
+    # Use trs that have data
+    trs = trs[1:]  # trs[0] contains only header info
+
+    # Get all links
+    links = []
+    prefix = 'https://www.pro-football-reference.com'
+    for td in trs:
+        hrefs = [link.get('href') for link in td.find_all('a')]
+        links.append(prefix + str(hrefs[0]))
+
+    return links
+
+
+def get_games_per_score(url):
+    """This function is meant to be iterated over each of the links returned
+       from the get_all_game_score_links() function."""
+
+    # Get page content
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Find the table
+    table = soup.find('table', attrs={'id': 'games'})  
+
+    # Get the games data
+    games = {}
+    for i, row in enumerate(table.find_all('tr')[1:]):
+        row_dict = {}  # re-initialize row_dict
+        for col in row.find_all('td'):
+            # Set row dictionary values
+            col_name = col['data-stat']
+
+            # Determine special columns
+            if col_name=='winner':
+                # winner_id
+                try:
+                    site = col.find('a').get('href')
+                    row_dict['winner_id'] = re.search(r'/teams/([A-Za-z0-9]+)/', site).group(1)
+                except:
+                    row_dict['winner_id'] = None
+            elif col_name=='loser':
+                # loser_id
+                try:
+                    site = col.find('a').get('href')
+                    row_dict['loser_id'] = re.search(r'/teams/([A-Za-z0-9]+)/', site).group(1)
+                except:
+                    row_dict['loser_id'] = None
+            elif col_name=='game_location':
+                # home_win
+                row_dict['home_win'] = 0 if r'@' in col.text else 1
+                continue
+            elif col_name=='boxscore_word':
+                # box_score
+                try:
+                    site = col.find('a').get('href')
+                    row_dict['boxscore'] = 'https://www.pro-football-reference.com'+site
+                except:
+                    row_dict['boxscore'] = None
+                continue
+            elif col_name=='game_outcome':
+                continue
+
+            row_dict[col_name] = col.text
+
+        # Add to games dictionary
+        games[i] = row_dict
+
+    return games
+    
+def get_data():
+    """Iterate over all links in get_all_game_score_links() function
+        to retrieve all basic NFL game data since 1920"""
+    # Look thru game score links
+    links = get_all_game_score_links()
+    games = {}
+    for i, link in enumerate(links):
+        # Print Status Update
+        win, loss = re.search(r'pts_win=(\d+)\&pts_lose=(\d+)', link).groups()
+        print(f'Retrieving {win}-{loss} Score....{i+1} of {len(links)}', end='\r')
+
+        # Dictionary for each score
+        new_games_raw = get_games_per_score(link)
+
+        # Rename keys so that no dupes exist
+        key_prefix = f'{i:04}'
+        new_games = {(key_prefix+str(k)): new_games_raw[k] 
+                        for k in new_games_raw.keys()}
+
+        # Combine old games and new games
+        games = {**games, **new_games}
+        time.sleep(2)
+
+    df = pd.DataFrame.from_dict(games, orient='index')
+    return df
+
+if __name__=='__main__':
+    df = get_data()
+
+    # Write to csv
+    today = datetime.datetime.now()
+    df.to_csv(f'data/all_nfl_games{today.year}_{today.month}_{today.day}.csv')
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 # NFL Data Exploration
-Initial goal of this repository is to explore how NFL Combine data relates to NFL success.
+Initial goal of this repository is to explore how NFL Combine data relates to NFL success.  Additionally, drawing conclusions on game-level data is another interest of this project.
 
 ## Installation
 
 Required dependencies (currently):
 
-- Python (>=3.4)
+- Python (>=3.6)
 - Numpy
 - Pandas
 - BeautifulSoup
@@ -14,9 +14,15 @@ Required dependencies (currently):
 
 Discovering hidden correlations between statistics in various sports has always been interesting to me. I especially am interested in motivations of NFL front offices regarding QB draft picks. This project will perform a deep dive into understanding why certain players are picked and which drafting methods yield the highest success rate.
 
+While investigating draft picks, it has become increasingly important to retreive additional game-level statistics to find correlated winners, losers and various other stats that revolve around betting.  This project has shifted from a narrow focus on QB draft picks and their successes (or failures) to a repository for retrieving a multitude of NFL statistics.
+
 ## Files
 
 **explore_data.ipynb**: This file is currently being used to test webscraping and data wrangling methods to grab NFL data pertinent to investigating how combine stats relate to NFL performance. Eventually, when the data is properly wrangled, this notebook will perform EDA, generate visuals, and test models.
+
+**all_game_data.ipynb**: The goal of this notebook is the setup the webscraper for retrieving scores from all games throughout the history of the NFL.  The only realized way to get this data (at least for free) is to start by getting all game scores (i.e. 20-17, 24-7, etc.) and scraping links to the list of all games at that score.  The scraping starts at this [pro football reference](https://www.pro-football-reference.com/boxscores/game-scores.htm) page and stores data from all the linked pages.
+
+**get_all_game_data.py**: This is the webscraper that pulls all game data and puts it into a dataframe and csv.  Running the file will create the csv while simply calling the get_data() function from another file will return a dataframe.
 
 ## Licensing and Acknowledgements
 
@@ -25,4 +31,5 @@ Data currently comes from multiple sources:
 - [NFL Combine.com](https://nflcombineresults.com/)
 - [Wikipedia](https://en.wikipedia.org/wiki/2021_NFL_Draft)
 - [Next Gen Stats](https://nextgenstats.nfl.com/)
+- [pro football reference](https://www.pro-football-reference.com)
 
